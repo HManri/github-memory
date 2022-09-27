fef18cae229657b80498c118bac7765d306ae78f
HManri <hector.manrique.prades@gmail.com>
2022-09-27 21:37:55 +0200

    add reset.css, globalstyles for styled-components and a default theme with some default values for colors, spacings, texts and more

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from 'globalStyles';
+import theme from 'theme/defaultTheme';
+import 'reset.css';
+import App from 'App';
 
 const root = ReactDOMClient.createRoot(document.getElementById('app'));
 
-root.render(<div>Hello world!</div>);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+);
